39c804a3978585f583182e062537d1afa20e5f90
James Bower <bower.james1996@gmail.com>
2026-06-29 21:21:21 +0100

    added provider model options

---
+++ packages/otter_ai_core/src/otter_ai_core/provider_api_model_options/povider_model_options.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+
+from .apis import KnownApis
+from .providers import KnownProviders
+
+class ProviderModelOption(BaseModel):
+    model: str
+    provider: KnownProviders
+    api: KnownApis
+    api_key: str | None
