bb380391d86de6728726b890e8139ffbf87e5149
James Bower <bower.james1996@gmail.com>
2026-08-16 15:10:41 +0100

    added resource acquisition and closing to TerminatingStream

--- packages/otter_ai_core/src/otter_ai_core/components/terminating_stream.py
+++ packages/otter_ai_core/src/otter_ai_core/components/terminating_stream.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
-from collections.abc import AsyncIterator
+from collections.abc import AsyncGenerator, AsyncIterator
+from contextlib import aclosing
 from typing import ClassVar
 
 
@@ -7,13 +8,14 @@ class TerminatingStream[TPartialEvent, TTerminalEvent](ABC):
     terminal_event_type: ClassVar[type[TTerminalEvent]]
 
     @abstractmethod
-    def _iterate_source(self) -> AsyncIterator[TPartialEvent | TTerminalEvent]: ...
+    def _iterate_source(self) -> AsyncGenerator[TPartialEvent | TTerminalEvent, None]: ...
 
     def __aiter__(self) -> AsyncIterator[TPartialEvent | TTerminalEvent]:
         return self._iterate_until_terminal_event()
 
     async def _iterate_until_terminal_event(self) -> AsyncIterator[TPartialEvent | TTerminalEvent]:
-        async for event in self._iterate_source():
-            yield event
-            if isinstance(event, self.terminal_event_type):
-                return
+        async with aclosing(self._iterate_source()) as source:
+            async for event in source:
+                yield event
+                if isinstance(event, self.terminal_event_type):
+                    return
