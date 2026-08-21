5c7d771717ebb57cd96668bcc32deac27100beb3
James Bower <james.bower@xanda.net>
2026-08-21 16:43:58 +0100

    Implement InMemorySessionManager state machine to satisfy session contract (green)

--- packages/otter_ai_core/src/otter_ai_core/session/__init__.py
+++ packages/otter_ai_core/src/otter_ai_core/session/__init__.py
@@ -3,10 +3,12 @@ from .contract import (
     SessionManagerContractCheck,
     SessionManagerFactory,
 )
+from .in_memory import InMemorySessionManager
 from .signature import SessionManager
 
 __all__ = [
     "SESSION_MANAGER_CONTRACT_CHECKS",
+    "InMemorySessionManager",
     "SessionManager",
     "SessionManagerContractCheck",
     "SessionManagerFactory",
